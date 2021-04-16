4b80ca7e0951b4fdcd0ce21a099d1d749170b536
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-16 22:04:07 +0000

    Update ec_create.c

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,1 +1,17 @@
 
+#include "hblk_crypto.h"
+
+EC_KEY *ec_create(void)
+{
+	EC_KEY *key;
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!key)
+		return (NULL);
+	if (!EC_KEY_generate_key(key))
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	return (key);
+}
