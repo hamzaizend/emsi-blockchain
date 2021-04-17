b46dd6d0569dc1a43a302c5051a733c4ed1b2573
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-17 08:47:02 -0700

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,1 +1,17 @@
+#include "openssl/crypto.h"
 
+void addBlock(int data){
+  if(head == null){
+    head = malloc(sizeof(struct block));
+    SHA256("",sizeof(""),head->prevHash);
+    head->blockData=data;
+    return ;
+  }
+  struct block *currentBlock=head;
+  while(currentBlock->link)
+    currentBlock=currentBlock->link;
+  struct block *newBlock=malloc(sizeof(struct block));
+  currentBlock->link=newBlock;
+  newBlock->blockData=data;
+  SHA256(toString(*currentBlock),sizeof(*currentBlock),newBlock->prevHash);
+}
