077aadd0007042257542b2dd5399cd719dce035c
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-17 07:51:40 -0700

    Update ec_save.c

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,7 +1,12 @@
-
 #include "hblk_crypto.h"
 
-
+/**
+ * ec_save - save existing EC key pair to disk
+ * @key: EC key pair to save
+ * @folder: path to folder to save keys
+ *
+ * Return: 1 on success, 0 on error
+ */
 int ec_save(EC_KEY *key, char const *folder)
 {
 	char buf[BUFSIZ];
