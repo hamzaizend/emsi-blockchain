cfd3a513dde4fd8df8b6c18a5cd14cdb571c6813
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-17 07:53:02 -0700

    Update ec_load.c

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,7 +1,11 @@
-
 #include "hblk_crypto.h"
 
-
+/**
+ * ec_load - load an EC key pair from the disk
+ * @folder: path to folder from which to load the keys
+ *
+ * Return: pointer to created EC key pair, NULL on error
+ */
 EC_KEY *ec_load(char const *folder)
 {
 	EC_KEY *key = NULL;
