f0c8d145c548cffaaeec19e156829ed43cf8203a
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-18 21:32:35 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -4,12 +4,16 @@
 #define GENESIS_DATA_LEN 16
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 /**Create a blockchain
-*The Blockchain must contain one block upon creation. This block is called the Genesis Block. Its content is static*/
+*The Blockchain must contain one block upon creation. This block is called the Genesis Block. Its content is static
+*/
 blockchain_t *blockchain_create(void){
-	blockchain_t *list=calloc(1,sizeof(*list));
+	
+	blockchain_t *list;
+	block_t *Genesis;
+	list=calloc(1,sizeof(*list));
 	if(list==NULL)
 	  return (NULL);
-	block_t *Genesis=calloc(1,sizeof(*Genesis));
+	Genesis=calloc(1,sizeof(*Genesis));
 	 if(Genesis==NULL){
 	  	free(Genesis);
 	  	return (NULL);
