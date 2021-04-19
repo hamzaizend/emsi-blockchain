95c653ef5c4671aa444dfbc6ac653d50e02fe168
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-19 11:19:03 +0000

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,24 +1,20 @@
 #include "blockchain.h"
-
 /**
  * block_create - create a Block structure and initializes it
  */
-block_t *block_create(block_t const *prev, int8_t const *data,
-		      uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
 {
-	block_t *block;
-
-	b = calloc(1, sizeof(*block));
-	if (block == NULL)
+	block_t *blk;
+	blk = calloc(1, sizeof(*blk));
+	if (blk == NULL)
 		return (NULL);
-
-	block->info.index = prev->info.index + 1;
-	block->info.difficulty = 0;
-	block->info.timestamp = time(NULL);
-	block->info.nonce = 0;
-	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
-	memcpy(block->data.buffer, data, data_len);
-	block->data.len = data_len;
-	memset(block->hash, 0, SHA256_DIGEST_LENGTH);
-	return (block);
+	blk->info.index = prev->info.index + 1;
+	blk->info.difficulty = 0;
+	blk->info.timestamp = time(NULL);
+	blk->info.nonce = 0;
+	memcpy(blk->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	memcpy(blk->data.buffer, data, data_len);
+	blk->data.len = data_len;
+	memset(blk->hash, 0, SHA256_DIGEST_LENGTH);
+	return (blk);
 }
