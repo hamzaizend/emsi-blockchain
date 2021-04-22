17392c917936018248363a626f1a124368a7a89f
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-22 18:18:54 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,11 +1,9 @@
 #include "blockchain.h"
 
-/**
- * block_hash - a function that computes the hash of a Block
- * Return: a pointer to hash_buf
- */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	sha256((int8_t const *)&(block->info), 4 + 4 + 8 + 8 + 32 + block->data.len, hash_buf);
-	return (hash_buf);
+memset(hash_buf, 0, SHA256_DIGEST_LENGTH);
+return (SHA256((const unsigned char *)block,
+	       sizeof(block->info) + block->data.len, hash_buf));
 }
