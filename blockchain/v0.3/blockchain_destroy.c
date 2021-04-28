484c502cb2db7999d7aa453079283edbab6fb10c
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-04-28 17:43:54 +0000

    Update blockchain_destroy.c

--- blockchain/v0.3/blockchain_destroy.c
+++ blockchain/v0.3/blockchain_destroy.c
@@ -1,3 +1,4 @@
+
 #include "blockchain.h"
 
 /**
@@ -9,5 +10,6 @@ void blockchain_destroy(blockchain_t *blockchain)
 	if (!blockchain)
 		return;
 	llist_destroy(blockchain->chain, 1, (void (*)(llist_node_t))block_destroy);
+	llist_destroy(blockchain->unspent, 1, free);
 	free(blockchain);
 }
