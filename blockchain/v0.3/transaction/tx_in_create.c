434223fc1eeae8fb0c824450261e68d46c9a44f2
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-05-08 19:57:08 +0000

    Update tx_in_create.c

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -1,21 +1,22 @@
 #include "transaction.h"
 
 /**
- * tx_in_create - creates tx input struct
- * @unspent: pointer to tx output struct
- * Return: ptr new tx input struct or NULL
+ * tx_in_create - allocate and initialize a transaction input structure
+ * @unspent: points to the unspent transaction output
+ * to be converted to a transaction input
+ *
+ * The created transaction input’s signature structure must be zeroed
+ * Return: pointer to the created tx_in, or NULL upon failure
  */
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 {
-	tx_in_t *txi;
+	tx_in_t *tx_in = calloc(1, sizeof(*tx_in));
 
-	if (!unspent)
+	if (tx_in == NULL || !unspent)
 		return (NULL);
-	txi = calloc(1, sizeof(*txi));
-	if (!txi)
-		return (NULL);
-	memcpy(txi->block_hash, unspent->block_hash, sizeof(txi->block_hash));
-	memcpy(txi->tx_id, unspent->tx_id, sizeof(txi->tx_id));
-	memcpy(txi->tx_out_hash, unspent->out.hash, sizeof(txi->tx_out_hash));
-	return (txi);
+	memcpy(tx_in->block_hash, unspent->block_hash,
+	       sizeof(tx_in->block_hash));
+	memcpy(tx_in->tx_id, unspent->tx_id, sizeof(unspent->tx_id));
+	memcpy(tx_in->tx_out_hash, unspent->out.hash, sizeof(tx_in->tx_out_hash));
+	return (tx_in);
 }
