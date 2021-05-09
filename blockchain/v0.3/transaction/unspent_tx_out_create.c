47d8af9eedb1fea3adcc51d89f432708f6c8ac19
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-05-09 22:53:03 +0000

    Update unspent_tx_out_create.c

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,23 +1,25 @@
 #include "transaction.h"
 
 /**
- * unspent_tx_out_create - creates unspent transaction struct
- * @block_hash: hash of block containing transaction
- * @tx_id: hash of transaction in block
- * @out: address of transaction output to be copied
+ * unspent_tx_out_create - allocate and initialize
+ * an unspent transaction output structure
+ * @block_hash: the hash of the Block where the referenced transaction output
+ * @tx_id: the hash of a transaction where the referenced transaction output
+ * @out: points to the referenced transaction output
  *
- * Return: pointer to created unspent transaction output upon success, or NULL
+ * Return: pointer to the created unspent transaction, or NULL upon failure
  */
 unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
 {
-	unspent_tx_out_t *utx = calloc(1, sizeof(*utx));
+	unspent_tx_out_t *unspent_tx_out_t = calloc(1, sizeof(*unspent_tx_out_t));
 
-	if (!utx)
+	if (unspent_tx_out_t == NULL)
 		return (NULL);
-	memcpy(utx->block_hash, block_hash, sizeof(utx->block_hash));
-	memcpy(utx->tx_id, tx_id, sizeof(utx->tx_id));
-	memcpy(&utx->out, out, sizeof(utx->out));
-	return (utx);
+	memcpy(unspent_tx_out_t->block_hash, block_hash,
+		sizeof(unspent_tx_out_t->block_hash));
+	memcpy(unspent_tx_out_t->tx_id, tx_id, sizeof(unspent_tx_out_t->tx_id));
+	memcpy(&unspent_tx_out_t->out, out, sizeof(unspent_tx_out_t->out));
+	return (unspent_tx_out_t);
 }
