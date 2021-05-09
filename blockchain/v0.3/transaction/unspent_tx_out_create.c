93aa74d8d4e0d9116decd149b5c55b2db7432a3c
hamzaizend <63814867+hamzaizend@users.noreply.github.com>
2021-05-09 14:10:55 +0000

    Update unspent_tx_out_create.c

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,14 +1,13 @@
 #include "transaction.h"
-#include <llist.h>
 
 /**
  * unspent_tx_out_create - creates unspent transaction struct
  * @block_hash: hash of block containing transaction
  * @tx_id: hash of transaction in block
  * @out: address of transaction output to be copied
- * Return: new unspent transaction struct
+ *
+ * Return: pointer to created unspent transaction output upon success, or NULL
  */
-
 unspent_tx_out_t *unspent_tx_out_create(
 	uint8_t block_hash[SHA256_DIGEST_LENGTH],
 	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
